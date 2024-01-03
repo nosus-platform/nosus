5d4d705ab870f333ee3a74a27d539d2f0e164fad
Tony Vi <winogradovaa@gmail.com>
2024-01-04 00:44:38 +0200

    chore: bundle analyzer

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { analyzer } from 'vite-bundle-analyzer';
+
+const plugins = [react()];
+
+if (process.env.ANALYZE) {
+    plugins.push(analyzer());
+}
 
 export default defineConfig({
-  plugins: [react()],
+  plugins,
 });
