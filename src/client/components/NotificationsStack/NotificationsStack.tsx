c47a00f4e71d978dd56d6bab7a5d0a39f4b5fc20
Tony Vi <winogradovaa@gmail.com>
2024-01-04 00:45:31 +0200

    chore: move out notifications stack

---
+++ src/client/components/NotificationsStack/NotificationsStack.tsx
@@ -0,0 +1,12 @@
+import { useContext } from 'react';
+import { Toaster } from 'sonner';
+import { pageContext } from '../../context/page';
+
+export interface ToastProps extends React.ComponentProps<typeof Toaster> {}
+
+// @see https://sonner.emilkowal.ski/styling
+export default (props: ToastProps) => {
+    const { theme } = useContext(pageContext);
+
+    return <Toaster {...props} theme={theme} />;
+};
